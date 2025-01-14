8eba5442de5fe60f992ab00c211d927987837f18
imake-ds-team <ugreenridg2@student.rccd.edu>
2025-01-14 11:20:51 -0800

    More style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-screen">
+    <div class="m-auto">
+        <h1>iMAKE Videos</h1>
+        <p>Create videos using generative AI.</p>
+        <div class="">
+            
+        </div>
+    </div>
+</div>
